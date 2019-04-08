e9dbc103153b7024d1d1d57c3b4e685fe56b499c
James Wu <therealjameswu@gmail.com>
2019-04-07 21:17:13 -0400

    Add basic bot command handler

--- src/blissbot.py
+++ src/blissbot.py
@@ -14,7 +14,9 @@ RTM_READ_DELAY_SECONDS = 1
 def parse_bot_commands(slack_events):
     for event in slack_events:
         if event['type'] == 'message' and not 'subtype' in event:
-                print(event['text'])
+                user_id = event['user']
+                message = event['text']
+                handle_bot_commands(user_id, message)
 
 def generate_message(constant_key, replacement_str):
         message_template = Messages[constant_key]
@@ -22,6 +24,17 @@ def generate_message(constant_key, replacement_str):
         message = re.sub(r'<\w+>', replacement_str, message_template)
 
         return message
+
+
+def handle_bot_commands(user_id, message):
+        if message.lower() == 'help':
+                text = generate_message('HELP_MESSAGE', user_id)
+                response = SLACK_BOT_CLIENT.api_call(
+                                'chat.postMessage',
+                                channel=user_id,
+                                text=text,
+                                as_user=True
+                        )
 
 
 def send_welcome_message():
@@ -33,24 +46,25 @@ def send_welcome_message():
                 user_name = user['name']
                 text = generate_message('WELCOME_MESSAGE', user_name)
                 print('Sending message to {}'.format(user_name))
-                SLACK_BOT_CLIENT.api_call(
-                        'chat.postMessage',
-                        channel=user_id,
-                        text=text,
-                        as_user=True                     
-                )
+                response = SLACK_BOT_CLIENT.api_call(
+                                'chat.postMessage',
+                                channel=user_id,
+                                text=text,
+                                as_user=True           
+                        )
 
 if __name__ == '__main__':
     if SLACK_BOT_CLIENT.rtm_connect(with_team_state=False):
         print('Connected to Bliss Bot!')
         BLISSBOT_ID = SLACK_BOT_CLIENT.api_call('auth.test')['user_id']
 
-        print('Sending welcome messages :-)')
-        send_welcome_message()
-    
-        while True:
-            parse_bot_commands(SLACK_BOT_CLIENT.rtm_read())
-            time.sleep(RTM_READ_DELAY_SECONDS)
+        # print('Sending welcome messages :-)')
+        # send_welcome_message()
+
+        if SLACK_BOT_CLIENT.rtm_connect():
+                while True:
+                        parse_bot_commands(SLACK_BOT_CLIENT.rtm_read())
+                        time.sleep(RTM_READ_DELAY_SECONDS)
   
     else:
         print("Connection failed :-(")
