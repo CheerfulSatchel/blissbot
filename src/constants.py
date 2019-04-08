d0ebfb9c37bcf511482e26ae78553928c4eb978a
James Wu <therealjameswu@gmail.com>
2019-04-07 21:17:31 -0400

    Add basic helper constant

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,10 @@
 Messages = {
-  'WELCOME_MESSAGE': 'Greetings, <user>! I hope you are having a wonderful day.'
+  'WELCOME_MESSAGE': 'Greetings, <user>! I hope you are having a wonderful day.',
+  'HELP_MESSAGE': '''
+    Blissbot, at your service! :smile:\n\nHere is a list of commands:
+       ```
+        help : You're reading it right now!
+        random : Retrieve a random story!
+       ```
+  '''
 }
