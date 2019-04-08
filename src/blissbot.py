308078a2a26d96dc9122e6502ee61b8ba01a0dec
James Wu <therealjameswu@gmail.com>
2019-04-08 17:34:30 -0400

    Refactor and serve random articles

--- src/blissbot.py
+++ src/blissbot.py
@@ -1,16 +1,14 @@
 import os
 import time
 import re
-from constants import Messages
-import re
+import requests
+import constants
 from slackclient import SlackClient
 
 SLACK_BOT_CLIENT = SlackClient(os.environ.get('SLACK_BOT_ACCESS_TOKEN'))
 WORKSPACE_USERS = []
 
 BLISSBOT_ID = None
-RTM_READ_DELAY_SECONDS = 1
-
 
 def parse_bot_commands(slack_events):
     for event in slack_events:
@@ -21,7 +19,7 @@ def parse_bot_commands(slack_events):
 
 
 def generate_message(constant_key, replacement_str):
-    message_template = Messages[constant_key]
+    message_template = constants.MESSAGE_TEMPLATES[constant_key]
     # Based on https://stackoverflow.com/questions/5658369/how-to-input-a-regex-in-string-replace
     message = re.sub(r'<\w+>', replacement_str, message_template)
 
@@ -29,19 +27,37 @@ def generate_message(constant_key, replacement_str):
 
 
 def handle_bot_commands(user_id, message):
+    reply_text = ''
     if message.lower() == 'help':
-        text = generate_message('HELP_MESSAGE', user_id)
-        response = SLACK_BOT_CLIENT.api_call(
-            'chat.postMessage',
-            channel=user_id,
-            text=text,
-            as_user=True
-        )
+        reply_text = generate_message('HELP_MESSAGE', user_id)
 
     elif message.lower() == 'random':
         # TODO: make API call to retrieve un-read news article
         print('Retrieving a new story for {}...'.format(user_id))
+        flask_response = requests.get(constants.API_BASE_ENDPOINT + 'random-story/', timeout=constants.API_TIMEOUT_MILLISECONDS)
+        if '20' in str(flask_response.status_code):
+            flask_response_json = flask_response.json()
+            if flask_response_json['ok']:
+                reply_text = flask_response_json['body']['link']
+        else:
+            # TODO: Provide an error message for the user
+            print('Nooose')
+    
+    elif message.lower() == 'example':
+        print('Just an example call...')
+        flask_response = requests.get(constants.API_BASE_ENDPOINT + 'example/', timeout=constants.API_TIMEOUT_MILLISECONDS)
+        print(flask_response.json())
 
+    if reply_text:
+        response = SLACK_BOT_CLIENT.api_call(
+                'chat.postMessage',
+                channel=user_id,
+                text=reply_text,
+                as_user=True
+            )
+    else:
+        # TODO: Send error reply
+        return
 
 def send_welcome_message():
     workspace_users_response = SLACK_BOT_CLIENT.api_call('users.list')
@@ -59,7 +75,6 @@ def send_welcome_message():
                 as_user=True
             )
 
-
 if __name__ == '__main__':
     if SLACK_BOT_CLIENT.rtm_connect(with_team_state=False):
         print('Connected to Bliss Bot!')
@@ -71,7 +86,7 @@ if __name__ == '__main__':
         if SLACK_BOT_CLIENT.rtm_connect():
             while True:
                 parse_bot_commands(SLACK_BOT_CLIENT.rtm_read())
-                time.sleep(RTM_READ_DELAY_SECONDS)
+                time.sleep(constants.RTM_READ_DELAY_SECONDS)
 
     else:
         printß("Connection failed :-(")
--- src/constants.py
+++ src/constants.py
@@ -1,10 +1,19 @@
-Messages = {
-  'WELCOME_MESSAGE': 'Greetings, <user>! I hope you are having a wonderful day.',
-  'HELP_MESSAGE': '''
-    Blissbot, at your service! :smile:\n\nHere is a list of commands:
-       ```
-        help : You're reading it right now!
-        random : Retrieve a random story!
-       ```
-  '''
+MESSAGE_TEMPLATES = {
+    'WELCOME_MESSAGE': 'Greetings, <user>! I hope you are having a wonderful day.',
+    'HELP_MESSAGE': '''
+      Blissbot, at your service! :smile:\n\nHere is a list of commands:
+        ```
+            help : You're reading it right now!
+            random : Retrieve a random story!
+        ```
+    '''
 }
+
+COMMANDS = {
+    'random',
+    'help'
+}
+
+API_BASE_ENDPOINT = 'http://d48c5536.ngrok.io/'
+API_TIMEOUT_MILLISECONDS = 2000
+RTM_READ_DELAY_SECONDS = 1
