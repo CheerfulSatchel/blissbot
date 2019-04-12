d1ffe4f4c8bbf421607919dff9fb008f80c51834
James Wu <therealjameswu@gmail.com>
2019-04-12 15:52:39 -0400

    Remove unneeded constants

--- src/constants.py
+++ src/constants.py
@@ -1,19 +1,3 @@
-MESSAGE_TEMPLATES = {
-    'WELCOME_MESSAGE': 'Greetings, <user>! I hope you are having a wonderful day.',
-    'HELP_MESSAGE': '''
-      Blissbot, at your service! :smile:\n\nHere is a list of commands:
-        ```
-            help : You're reading it right now!
-            random : Retrieve a random story!
-        ```
-    '''
-}
-
-COMMANDS = {
-    'random',
-    'help'
-}
-
-API_BASE_ENDPOINT = 'http://250448ab.ngrok.io/'
+API_BASE_ENDPOINT = 'http://39b95662.ngrok.io/'
 API_TIMEOUT_SECONDS = 2
 RTM_READ_DELAY_SECONDS = 1
